04f9a0e6bb9248ef82fb38a9060f0ca97b3f3711
Dmytro <dmitry.dvernikov@gmail.com>
2025-06-20 18:18:20 +0300

    fix app/notes/[id]/page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,12 +2,6 @@ import {dehydrate, HydrationBoundary, QueryClient} from '@tanstack/react-query';
 import {fetchNoteById} from '@/lib/api';
 import NoteDetailsClient from './NoteDetails.client';
 
-type PageProps = {
-    params: {
-        id: string;
-    };
-};
-
 export default async function NoteDetailsPage({params}: { params: { id: string } }) {
     const id = Number(params.id);
     const queryClient = new QueryClient();
