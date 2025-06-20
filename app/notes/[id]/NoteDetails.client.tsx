73a27ec86a6f88b95f5826903a255fbf0296614f
Dmytro <dmitry.dvernikov@gmail.com>
2025-06-20 18:06:14 +0300

    fix NoteDetails.client.tsx

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -13,6 +13,7 @@ export default function NoteDetailsClient() {
     const {data: note, isLoading, error} = useQuery<Note>({
         queryKey: ['note', id],
         queryFn: () => fetchNoteById(id),
+        refetchOnMount: false,
     });
 
     if (isLoading) return <p>Loading, please wait...</p>;
